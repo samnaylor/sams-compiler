645b8c60fea1c3e394152b0db33ac1274ae0a1ca
Isaac Naylor <I.acnaylor@gmail.com>
2022-08-21 10:15:30 +0100

    updated styling in run

--- run.py
+++ run.py
@@ -1,16 +1,18 @@
 from argparse import ArgumentParser
 from subprocess import CalledProcessError, check_output
 
+
 def main() -> None:
     argparser = ArgumentParser()
-    argparser.add_argument("name", type = str, help = "The program to be run and checked.")
+    argparser.add_argument("name", type=str, help="The program to be run and checked.")
     args = argparser.parse_args()
 
     try:
         check_output([f"./bin/{args.name}"])
-        out = check_output(["echo", "$?"])
+        check_output(["echo", "$?"])
     except CalledProcessError as cpe:
         print("Program returned code: %d" % cpe.returncode)
+
 
 if __name__ == "__main__":
     main()
